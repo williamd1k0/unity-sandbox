0a2df52dbdd505fdc643d058266941616a3ecd7e
William Tumeo <tumeowilliam@gmail.com>
2016-10-28 01:29:49 -0200

    First rigid body impulse test

---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public Rigidbody2D body;
+	public float jumpSpeed = 30;
+	private bool jumping = false;
+
+	// Use this for initialization
+	void Start () {
+		print (body);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.X) && !jumping){
+			print("Jump");
+			jumping = true;
+			body.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+		}
+	}
+}
